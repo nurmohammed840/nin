d4d2c1e1a8461ac6ade3c46250ddbaf01dfbbad6
Nur Mohammed <nurmohammed840@gmail.com>
2026-09-26 20:06:54 +0600

    removed `egui`

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,2 @@
-pub mod app;
-
 pub mod discover;
 pub mod file;
-
-#[cfg(target_os = "android")]
-#[unsafe(no_mangle)]
-fn android_main(app: winit::platform::android::activity::AndroidApp) {
-    // android_logger::init_once(
-    //     android_logger::Config::default()
-    //         .with_max_level(log::LevelFilter::Info),
-    // );
-
-    let options = eframe::NativeOptions {
-        android_app: Some(app),
-        ..Default::default()
-    };
-
-    eframe::run_native(
-        "Nin",
-        options,
-        Box::new(|_cc| Ok(Box::new(app::MyApp::default()))),
-    )
-    .unwrap();
-}
